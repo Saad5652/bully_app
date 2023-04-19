f8a5d2ec4895f53b774019632ee6c4014b4ffbec
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-04-19 23:02:41 +0100

    fix correct phone number field in parent serializer

--- actors/serializers.py
+++ actors/serializers.py
@@ -2,19 +2,20 @@ from rest_framework import serializers
 
 from .models import Parent, Teacher, Student
 
+
 class ParentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parent
-        fields = ['id', 'name', 'phone',]
+        fields = ['id', 'name', 'phone_number',]
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ['id', 'name', 'image',]
 
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'name', 'parent', 'division', 'image', 'gender', ]
-
-
