b40ffd79e7831f83dc27fbb1559a62398ce78286
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2023-04-19 23:32:39 +0100

    feat update media settings

--- bully_report/settings.py
+++ bully_report/settings.py
@@ -137,6 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATIC FILES
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # CORS HEADERS
 
--- bully_report/urls.py
+++ bully_report/urls.py
@@ -5,13 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    
+
     path('api/v1/auth/', include('rest_framework.urls')),
     path('api/v1/', include('actors.urls')),
     path('api/v1/', include('entities.urls')),
     path('api/v1/', include('reports.urls')),
 
-    path('admin/', admin.site.urls), 
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
